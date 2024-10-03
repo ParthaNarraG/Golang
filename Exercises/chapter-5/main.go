33a49b2c98bb21ccc16a3358b11c6499d3d91ce7
ParthaNarraG <partha.narra123@gmail.com>
2024-10-03 21:41:43 +0530

    functions exercises one problem solved

--- Exercises/chapter-5/main.go
+++ Exercises/chapter-5/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Functions Exercises")
+	var length, _ = fileLen("D:/Golang/Control Statements/main.go")
+	fmt.Println("length", length)
+}
+
+// Write a function called fileLen that has an input parameter of type string and returns an int and an error.
+// The function takes in a filename and returns the number of bytes in the file.
+// If there is an error reading the file, return the error. Use defer to make sure the file is closed properly.
+
+func fileLen(fileName string) (int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("error", err)
+		return 0, err
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	return int(fileInfo.Size()), nil
 }
